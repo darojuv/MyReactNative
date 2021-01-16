429c1931cafeefb0b9b7e4de7f9d7b9bd3929cc9
darojuv@gmail.com <darojuv@gmail.com>
2021-01-16 12:13:24 -0800

    next would be to myreactnative

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
- 
+  
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
